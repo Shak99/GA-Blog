137674ce8a4971bb33b8b9d37f9f4cd1c96ca6ea
Shakira Lewis <shakiramlewis@gmail.com>
2022-04-25 16:37:00 -0700

    Commit Part 2 of Lab

--- tvcollector/main_app/models.py
+++ tvcollector/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Tvlist(models.Model):
+    title = models.CharField(max_length=250)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    seasons = models.IntegerField()
+    description = models.CharField(max_length=350)
+    status = models.CharField(max_length=250)
+    
+    def tv_index(request):
+        shows = Tvlist.objects.all()
+        return render(request, 'tv/index.html', {'shows': shows})
